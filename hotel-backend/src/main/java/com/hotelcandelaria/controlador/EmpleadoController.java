b1fd92e789c059b06f723feef7a09ce148c484db
StevenTuestaM <steven_tuesta@hotmail.com>
2026-07-20 01:40:35 -0500

    Update EmpleadoController.java

--- hotel-backend/src/main/java/com/hotelcandelaria/controlador/EmpleadoController.java
+++ hotel-backend/src/main/java/com/hotelcandelaria/controlador/EmpleadoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/empleados")
+@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class EmpleadoController {
 
